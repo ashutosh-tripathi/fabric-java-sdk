d6fcbc182ab22fa7fc6b862292cb25b9801f0f7b
ashutosh-tripathi <rbj.ashu@gmail.com>
2023-03-18 13:08:05 +0530

    Postman collection

--- src/main/java/com/fabric/client/ClientApplication.java
+++ src/main/java/com/fabric/client/ClientApplication.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.logging.log4j.LogManager ;
-import org.apache.logging.log4j.Logger ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,7 +13,6 @@ import com.google.gson.Gson;
 @SpringBootApplication(scanBasePackages = {"com.*"})
 public class ClientApplication {
 	
-	private static final Logger logger = LogManager.getLogger(ClientApplication.class);
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ClientApplication.class, args);
 		logger.debug("Fabric Java SDK started");
